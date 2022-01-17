c983aa6e22f267f91c4b8b1867e3c2d31016239a
huilin <lihuilin023@gmail.com>
2022-01-17 19:04:53 +0100

    commit

--- UNIOA_Framework/Init_X.py
+++ UNIOA_Framework/Init_X.py
@@ -3,7 +3,7 @@ import numpy as np
 class Init_X:
     @staticmethod
     def Init_X(M, n, lb_x, ub_x):
-        np.random.seed(2)
         X = np.random.uniform(lb_x, ub_x, (M, n))
         return X
 
+# https://universiteitleiden.zoom.us/j/2673710181?pwd=eWdrOUloWTcvSDFvVTJ2TzgwdTBvdz09
