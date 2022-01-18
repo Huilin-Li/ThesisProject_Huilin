2478baf854e98a819dae366c37241d1314e715a0
huilin <lihuilin023@gmail.com>
2022-01-18 21:49:38 +0100

    commit

--- SEP_implement/SEP_PSO_fix.py
+++ SEP_implement/SEP_PSO_fix.py
@@ -2,16 +2,6 @@ import numpy as np
 from UNIOA_Framework.NatureOpt import NatureOpt
 
 
-# -------------------------------------------------------------------------------------------
-# The file is a translation of Crow-Optimizer\cite{1,2} from MatLab to Python.
-# This Python version code suits our specific experiment cases in IOHanalyzer\cite{3}.
-# The execution logic is same as the original implementation in \cite{2}.
-# -------------------------------------------------------------------------------------------
-# References:
-# [1]A. Askarzadeh, ‘A novel metaheuristic method for solving constrained engineering optimization problems: Crow search algorithm’, Computers & Structures, vol. 169, pp. 1–12, Jun. 2016, doi: 10.1016/j.compstruc.2016.03.001.
-# [2]https://nl.mathworks.com/matlabcentral/fileexchange/57867-crow-search-algorithm-for-constrained-optimization?focused=6496133&s_tid=gn_loc_drop&tab=function
-# [3]C. Doerr, H. Wang, F. Ye, S. van Rijn, and T. Bäck, ‘IOHprofiler: A Benchmarking and Profiling Tool for Iterative Optimization Heuristics’, arXiv:1810.05281 [cs], Oct. 2018, Accessed: Sep. 19, 2021. [Online]. Available: http://arxiv.org/abs/1810.05281
-# -------------------------------------------------------------------------------------------
 
 class PSO_SEP_fix(NatureOpt):
     def __init__(self, func, hyperparams_set, budget_factor=1e4):
